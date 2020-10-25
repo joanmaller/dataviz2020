775168b24cbcbdf02a2f3dc8bb7ba7a3d3cffaf9
joanmaller <joanmaller@gmail.com>
2020-10-25 02:50:27 +0100

    Fet els 3 geoJsons amb punts i addpointsjson cutre, Problema que els geojson són molt llargs

--- addjsonpoints.py
+++ addjsonpoints.py
@@ -1,6 +1,7 @@
 import json
 import geojson
 import csv
+import copy
 import random
 
 from osgeo import ogr
@@ -16,90 +17,92 @@ for feature in data['features']:
         # Poligon del barri 1 serà guardat a pos 0 de poligonsbarris
         poligonsbarris.append(ogr.CreateGeometryFromJson(geom)) 
 
-"""
+
+
 duracions = [0,1,2]
 dictduracions = {0:"Fins a 6 mesos", 1:"De 6 a 12 mesos", 2:"Més de 12 mesos"}
 inv_dictduracions = {"Fins a 6 mesos":0, "De 6 a 12 mesos":1, "Més de 12 mesos":2}
-
-numatur_durada1 = [{}*9]
-
-with open('2020_Durada_atur.csv', 'r') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
-	line_count = 0
-
-		
-	#S'ha de guardar coordenades, mes i duracio.
-	for row in csv_reader:
-		indx_durada = inv_dictduracions[row[6]]
-
-		if line_count == 0:
-			pass
-		elif int(row[4]) == 1: #Inicialitzar variables
-
-			if indx_durada == 0:
-				numatur_durada1[int(row[4])][int(row[1])] = [int(row[7])]
-			elif indx_durada == 1:
-				numatur_durada2[int(row[4])][int(row[1])] = [int(row[7])]
-			else:
-			# row[1] mes
-			# row[4] codi Barri
-			# row[6] durada
-			# row[7] número de punts
-		else :
-			numaturdict[int(row[4])][int(row[1])] = [int(row[7])]
-			
-		line_count += 1
-
-
-"""
-b = 1
-m = 1
-polygon = poligonsbarris[b-1]
-env = polygon.GetEnvelope()
-xmin, ymin, xmax, ymax = env[0],env[2],env[1],env[3]
-
-num_points = 1000
-
-counter = 0
-
-multipoint = ogr.Geometry(ogr.wkbMultiPoint)
-
-
-for i in range(num_points):
-    while counter < num_points:
-
-        point = ogr.Geometry(ogr.wkbPoint)
-        point.AddPoint(random.uniform(xmin, xmax),
-                       random.uniform(ymin, ymax))
-
-        if point.Within(polygon):
-
-            multipoint.AddGeometry(point)
-
-            counter += 1
-
-feature_barri = multipoint.ExportToJson()
-print("Feature Barri:")
-print(feature_barri)
-
+aturats_barri = {}
 
 
 #Read geoJson header
 with open('points_duracioatur.geojson', 'r') as nf:
     newdata = json.load(nf)
 
-newdata['features'].append(feature_barri)
-'''
-#Loop over GeoJSON features and add the new points
-newdata = data
-feature = feature_barri
-print(feature["type"])
-feature['properties'] = {}
-feature['properties']["Mes"] = m
-feature['properties']["Codi_Barri"] = b
-newdata['features'].append(feature)
-'''
+with open('2020_Durada_atur.csv', 'r') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+
+	#S'ha de guardar coordenades, mes i duracio.
+	feature_barri_base ={"type": "Feature", "properties" :{}, "geometry" : {"type": "MultiPoint", "coordinates": []} }
+	feature_barris = {}
+	line_count = 0
+	for row in csv_reader:
+		if (line_count != 0 and inv_dictduracions[row[6]] == 2):
+
+			num_barri = int(row[4])
+			num_mes = int(row[1])
+			print(num_barri ,"  " , num_mes)
+
+			if num_barri != 99 and num_barri != "Codi_Barri": #Codi de barri diferent de no consta
+				
+				polygon = poligonsbarris[num_barri -1]
+				env = polygon.GetEnvelope()
+				xmin, ymin, xmax, ymax = env[0],env[2],env[1],env[3]
+				num_aturats = int(row[7])
+
+				if num_mes == 1:
+					aturats_barri[num_barri] = num_aturats
+					feature_barris[num_barri] = copy.deepcopy(feature_barri_base)
+					feature_barris[num_barri]["properties"]["Barri"] = num_barri
+					
+					counter = 0
+					for i in range(num_aturats):
+					    while counter < num_aturats:
+					        point = ogr.Geometry(ogr.wkbPoint)
+					        point.AddPoint(random.uniform(xmin, xmax),
+					                       random.uniform(ymin, ymax))
+					        if point.Within(polygon):
+					            feature_barris[num_barri]["geometry"]["coordinates"].append([point.GetX(),point.GetY()])
+					            counter += 1
+					feat = feature_barris[num_barri]
+					
+					coordenades = feature_barris[num_barri]["geometry"]["coordinates"]
+					
+				else:
+	
+					feat = feature_barris[num_barri]
+					coordenades = feature_barris[num_barri]["geometry"]["coordinates"]
+					#Si el num nou d'aturats es més baix seleccionar una part dels aturats màxims
+
+					if num_aturats < aturats_barri[num_barri]:
+						coordenades = coordenades[0:num_aturats]
+
+					else:
+						num_points = num_aturats - aturats_barri[num_barri]
+						aturats_barri[num_barri] = num_aturats
+
+						counter = 0
+						for i in range(num_points):
+						    while counter < num_points:
+						        point = ogr.Geometry(ogr.wkbPoint)
+						        point.AddPoint(random.uniform(xmin, xmax),
+						                       random.uniform(ymin, ymax))
+						        if point.Within(polygon):
+						            coordenades.append([point.GetX(),point.GetY()])
+						            counter += 1
+
+						feature_barris[num_barri]["geometry"]["coordinates"] = coordenades            	
+
+				feat["geometry"]["coordinates"] = coordenades
+				feat["properties"]["Mes"] = num_mes
+				print(len(feature_barris[num_barri]["geometry"]["coordinates"]))
+				newdata['features'].append(feat)
+
+
+		line_count += 1
+
 #Write result to a new file
-with open('points_duracioaturnew.geojson', 'w') as n2f:
+with open('points_duracioatur2.geojson', 'w') as n2f:
     json.dump(newdata, n2f)
 
