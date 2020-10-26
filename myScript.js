d3800096a02aa53446eede7ab7cad7f84cefe856
Jaume Prats <jaumepratscris@gmail.com>
2020-10-26 23:25:11 +0100

    Fitxers finals

--- myScript.js
+++ myScript.js
@@ -10,6 +10,18 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function() {
+    
+    document.getElementById('content').style.display = 'none';
+    
+    var coll = document.getElementsByClassName("collapsible");
+    coll[0].addEventListener('click', function(e) {
+        this.classList.toggle('active');
+        var cont = document.getElementById('content')
+        cont.style.display = (cont.style.display === 'block') ? 'none' : 'block';
+        
+
+      });
+    
   map.addLayer({
     id: 'points_duracioatur0',
     type: 'circle',
@@ -19,9 +31,9 @@ map.on('load', function() {
       data: './points_duracioatur0.geojson' // replace this with the url of your own geojson
     },
     paint: {
-      'circle-radius': 2,
+      'circle-radius': 3,
       'circle-color': '#00FF00',
-      'circle-opacity': 0.5
+      'circle-opacity': 0.25
     }
   });
 
@@ -34,9 +46,9 @@ map.on('load', function() {
       data: './points_duracioatur1.geojson' // replace this with the url of your own geojson
     },
     paint: {
-      'circle-radius': 2,
+      'circle-radius': 3,
       'circle-color': '#FF0000',
-      'circle-opacity': 0.5
+      'circle-opacity': 0.25
     }
   });
 
@@ -49,58 +61,42 @@ map.on('load', function() {
       data: './points_duracioatur2.geojson' // replace this with the url of your own geojson
     },
     paint: {
-      'circle-radius': 2,
+      'circle-radius': 3,
       'circle-color': '#0000FF',
-      'circle-opacity': 0.5
+      'circle-opacity': 0.15
     }
   });
+  
+  mesos = ['Gener', 'Febrer', 'Març', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Setembre', 'Octubre', 'Novembre', 'Desembre']
 
   //Time Slide Bar
     document.getElementById('slider').addEventListener('input', function(e) {
 		var month = parseInt(e.target.value);
-		// update the map
+
 	  	map.setFilter('points_duracioatur0', ['==', ['number', ['get', 'Mes']], month]);
 	  	map.setFilter('points_duracioatur1', ['==', ['number', ['get', 'Mes']], month]);
 	  	map.setFilter('points_duracioatur2', ['==', ['number', ['get', 'Mes']], month]);
-	 	//filterHour = ['==', ['number', ['get', 'Hour']], hour];
-		//map.setFilter('collisions', ['all', filterHour, filterDay]);
 
 	  	// update text in the UI
-	  	document.getElementById('active-hour').innerText = month;
+	  	document.getElementById('active-month').innerText = mesos[month-1];
 	});
+	
+    
    
-	//Filtre durada0
-	//document.getElementById('filter0').addEventListener('change', function(e) {
-		//var checked = e.target.checked;
-		//if (checked) {
-			//map.setLayoutProperty('durada0', 'visibility', 'visible');
-		//} else {
-			//map.setLayoutProperty('durada0', 'visibility', 'none');
-		//}
-	//}
-	
-	/*
-	document.getElementById('filters').addEventListener('change', function(e) {
-		var durada = e.target.value;
-	  	// update the map filter
-	  	if (durada === 'durada0') {
-	  		map.getLayer(durada0).
-
-	  	}
-	    //	filterDay = ['!=', ['string', ['get', 'Day']], 'placeholder'];
-	    if (day === 'all') {
-		  // `null` would not work for combining filters
-		  filterDay = ['!=', ['string', ['get', 'Day']], 'placeholder'];
-		} else if (day === 'weekday') {
-	    	filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
-	  	} else if (day === 'weekend') {
-	    	filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], true, false];
-	  	} else {
-	    	console.log('error');
-	  	}
-	  	map.setFilter('collisions', ['all', filterHour, filterDay]);
+	document.getElementById('filter0').addEventListener('change', function(e) {
+	    map.setLayoutProperty('points_duracioatur0','visibility', e.target.checked ? 'visible' : 'none');
 	});
-	*/
 	
+	document.getElementById('filter1').addEventListener('change', function(e) {
+	    map.setLayoutProperty('points_duracioatur1','visibility', e.target.checked ? 'visible' : 'none');
+	});
+	
+	document.getElementById('filter2').addEventListener('change', function(e) {
+	    map.setLayoutProperty('points_duracioatur2','visibility', e.target.checked ? 'visible' : 'none');
+	});
+	
+	
+
+    
 });
 
